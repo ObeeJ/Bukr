97d374ddd52f666330af4532410b870164dba960
Ajayi ObaniJesu Toluwanimi <ajayioba2000@gmail.com>
2025-09-04 12:05:11 +0100

    Fix deployment issues: remove missing logo references and service worker

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,5 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import './components/ui/button.css'
-import * as serviceWorker from './serviceWorker'
 
 createRoot(document.getElementById("root")!).render(<App />);
-
-// Register service worker for PWA functionality
-serviceWorker.register();
-import { register } from './service-worker';
-
-register({
-  onSuccess: (registration) => {
-    console.log('Service worker successfully registered:', registration);
-  },
-  onUpdate: (registration) => {
-    console.log('Service worker update available:', registration);
-  },
-});
